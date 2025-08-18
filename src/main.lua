a020f2e1cdd2f44d9de000253ade33a13b5aa8a7
Seth Anderson <seth.c.anderson@gmail.com>
2025-08-18 13:18:48 -0500

    chore: scaffold pico-8 team-friendly project (src/main.lua)

---
+++ src/main.lua
@@ -0,0 +1,14 @@
+-- src/main.lua
+-- pico-8 entry points
+function _init()
+  -- runs once at start
+end
+
+function _update60()
+  -- runs 60 times a second
+end
+
+function _draw()
+  cls()
+  print("hello pico-8 team build!", 10, 10)
+end
